a931adc2c1e80a77a951e3670911603be1369556
Lomari <Odamilola36@gmail.com>
2022-10-19 12:23:37 +0100

    configure swagger and remove deprecated dependencies

--- src/main/java/com/lomari/walletapp/config/SwaggerConfig.java
+++ src/main/java/com/lomari/walletapp/config/SwaggerConfig.java
@@ -7,29 +7,50 @@ import org.springframework.context.annotation.Import;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spi.service.contexts.SecurityContextBuilder;
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
-@Deprecated
 @Configuration
-@EnableSwagger2WebMvc
 @Import({SpringDataRestConfiguration.class, BeanValidatorPluginsConfiguration.class})
 public class SwaggerConfig {
+
+    static final String AUTHENTICATION_HEADER = "Authorization";
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.OAS_30)
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(List.of(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(getApiInfo());
     }
+
+    private ApiKey apiKey() {
+        return new ApiKey(AUTHENTICATION_HEADER, "JWT", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope scope = new AuthorizationScope("global", "accessEverything");
+        return List.of(new SecurityReference(AUTHENTICATION_HEADER, new AuthorizationScope[]{scope}));
+    }
+
 
     private ApiInfo getApiInfo() {
         return new ApiInfo(
